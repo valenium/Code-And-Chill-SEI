a671f7362f02c5f7e16b883f33d99aace3145c0f
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-11-20 09:36:21 -0500

    Add partials folder and connect routes to controllers and controllers to models

--- express-recipes/routes/recipes.js
+++ express-recipes/routes/recipes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const recipesCtrl = require('../controllers/recipes-controller')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
