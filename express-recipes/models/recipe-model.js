27206852348113e7e0b6074a6f96829358cb6b80
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-11-20 09:48:28 -0500

    Add recipe model

--- express-recipes/models/recipe-model.js
+++ express-recipes/models/recipe-model.js
@@ -1,2 +1,29 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
+const recipeSchema = new Schema ({
+    name: {
+        type: String,
+    },
+    category: {
+        type: String,
+        enum: ['Breakfast', 'Lunch', 'Dinner'],
+        default: 'Breakfast'
+    },
+    description: {
+        type: String,
+    },
+    instructions: {
+        type: String,
+    },
+    ingredients: {
+        type: String,
+    },
+    comments: {
+        type: String
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Recipe', recipeSchema)
